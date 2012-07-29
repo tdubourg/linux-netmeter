2cd69018ed664f53bd2edca3522baf8757bc0448
Troll <trolldu26@gmail.com>
2012-07-29 16:15:01 +0200

    Ready for release. Debugged. Configurable. Licensing included

--- netmeter_config.py
+++ netmeter_config.py
@@ -1,5 +1,20 @@
-# Constants: 
+# Copyright (C) 2012  tdubourg, License: see LICENSE file in same folder
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License v3
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import re
+
+# Constants: 
 DBG = True
 
 LOG_DIRECTORY = "/tmp"
@@ -15,6 +30,11 @@ FNAME_MONITOR = "/proc/net/dev"
 
 INTERVAL = 30 # update every x seconds
 
+# Utilities: 
+
+r_interface_line = re.compile("^ *([a-zA-z0-9]+):(( +([0-9]+)){16})\n?$")
+r_figure = re.compile("([0-9]+)")
+
 def get_interfaces():
 	interfaces = []
 	with open(FNAME_MONITOR) as f:
@@ -25,5 +45,5 @@ def get_interfaces():
 	return interfaces
 
 def fname_with_interface(fname, interface_name):
-	return fname.replace(FNAME_EXT, interface_name + FNAME_EXT)
+	return fname.replace(FNAME_EXT, "_" + interface_name + FNAME_EXT)
 
